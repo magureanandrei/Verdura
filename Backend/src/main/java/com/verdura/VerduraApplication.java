2ee56a382fbaf412a326f7fced406b594cb3cfc3
magureanandrei <andimagurean714@gmail.com>
2025-08-29 17:12:30 +0300

    feat_InitialCommit

---
+++ Backend/src/main/java/com/verdura/VerduraApplication.java
@@ -0,0 +1,13 @@
+package com.verdura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerduraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerduraApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/verdura/VerduraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verdura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerduraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
